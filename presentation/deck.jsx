59af16df8db07216fa7d0c4b90ca6573799d9888
ALFer <elf.er.is@gmail.com>
2015-11-07 10:50:29 +0200

    Presentation complete

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -2,119 +2,187 @@ import React from "react/addons";
 
 import {
   Appear, BlockQuote, Cite, CodePane, Deck, Fill,
-  Heading, Image, Layout, Link, ListItem, List, Quote, Slide, Text
+  Heading, Image, Layout, Link, ListItem, List, Quote, Slide, Text, S
 } from "../src/spectacle";
 
 import preloader from "../src/utils/preloader";
 
-import Interactive from "./interactive";
 
 const images = {
-  city: require("./city.jpg"),
-  kat: require("./kat.png"),
-  logo: require("./formidable-logo.svg")
+  alf: require("./alf.jpg"),
+  react: require("./logo_react.svg"),
+  flux: require("./logo_flux.svg")
 };
 
-preloader([images.city, images.kat]);
+preloader([images.alf, images.react, images.flux]);
 
 export default class extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={800}>
+      <Deck transition={["zoom", "slide"]} transitionDuration={800} progress="bar">
+
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="black">
-            Spectacle
+            Почему
           </Heading>
           <Heading size={1} fit caps>
-            A ReactJS Presentation Library
+            React и Flux
           </Heading>
           <Heading size={1} fit caps textColor="black">
-            Where You Can Write Your Decks In JSX
-          </Heading>
-          <Link href="https://github.com/FormidableLabs/spectacle">
-            <Text bold caps textColor="tertiary">View on Github</Text>
-          </Link>
-          <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
-        </Slide>
-        <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-          <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-          <Heading size={1} fit textColor="primary" textFont="secondary">
-            Wait what?
+            это хорошо
           </Heading>
         </Slide>
-        <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-          <CodePane
-            lang="javascript"
-            source={require("raw!./deck.example")}
-            margin="20px auto"/>
-        </Slide>
-        <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-          <Appear fid="1">
-            <Heading size={1} caps fit textColor="primary">
-              Full Width
-            </Heading>
-          </Appear>
-          <Appear fid="2">
-            <Heading size={1} caps fit textColor="tertiary">
-              Adjustable Darkness
-            </Heading>
-          </Appear>
-          <Appear fid="3">
-            <Heading size={1} caps fit textColor="primary">
-              Background Imagery
-            </Heading>
-          </Appear>
-        </Slide>
-        <Slide transition={["zoom", "fade"]} bgColor="primary">
-          <Heading caps fit>Flexible Layouts</Heading>
+
+        <Slide transition={["slide"]} bgColor="black">
           <Layout>
             <Fill>
-              <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Left
-              </Heading>
+              <Image src={images.alf.replace("/", "")} height="293px"/>
             </Fill>
             <Fill>
-              <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                Right
-              </Heading>
+                <Heading size={1} fit textColor="primary">
+                    Илья Сегеда <S type="italic">aka ALFer</S>
+                </Heading>
+                <List textColor="primary">
+                    <ListItem>В фронтенде с 2001</ListItem>
+                    <ListItem>Занимался Web/Flash до 2010</ListItem>
+                    <ListItem>Занимаюсь JavaScript с 2007</ListItem>
+                </List>
+                <List textColor="primary">
+                    <ListItem>Twitter: <Link href="http://twitter.com/ALF_er">@ALF_er</Link></ListItem>
+                    <ListItem>Чаты: Illia Segeda aka ALFer</ListItem>
+                </List>
             </Fill>
           </Layout>
         </Slide>
-        <Slide transition={["slide"]} bgColor="black">
-          <BlockQuote>
-            <Quote>Wonderfully formatted quotes</Quote>
-            <Cite>Ken Wheeler</Cite>
-          </BlockQuote>
-        </Slide>
-        <Slide transition={["slide", "spin"]} bgColor="primary">
-          <Heading caps fit size={1} textColor="tertiary">
-            Smooth
+
+        <Slide transition={["zoom", "fade"]} bgColor="primary">
+          <Heading size={1} fit>
+            Немного истории
           </Heading>
-          <Heading caps fit size={1} textColor="secondary">
-            Combinable Transitions
-          </Heading>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <List>
-            <ListItem><Appear fid="1">Inline style based theme system</Appear></ListItem>
-            <ListItem><Appear fid="2">Autofit text</Appear></ListItem>
-            <ListItem><Appear fid="3">Flexbox layout system</Appear></ListItem>
-            <ListItem><Appear fid="4">React-Router navigation</Appear></ListItem>
-            <ListItem><Appear fid="5">PDF export</Appear></ListItem>
-            <ListItem><Appear fid="6">And...</Appear></ListItem>
+            <ListItem>В 2003 Brad Fitzpatrick выпустил Memcached, и показал как устроен LiveJournal</ListItem>
+            <ListItem>В 2004 Google опубликовал описание MapReduce, и показал как обрабатывать данные</ListItem>
+            <ListItem>В 2007 Amazon опубликовал описание Dynamo, и показал как строить БД</ListItem>
+            <ListItem>В 2010 Twitter свитчнулся на client-side templating, превратив сервер в просто API</ListItem>
+            <ListItem><Appear>В 2013 Facebook презентует React</Appear></ListItem>
           </List>
+
+          {/*<CodePane
+            lang="javascript"
+            source={require("raw!./deck.example")}
+            margin="20px auto"/>*/}
         </Slide>
-        <Slide transition={["slide"]} bgColor="primary">
-          <Heading size={1} caps fit textColor="tertiary">
-            Your presentations are interactive
-          </Heading>
-          <Interactive/>
+
+        <Slide transition={["slide"]} bgColor="black">
+            <Image src={images.react} height="400px" margin="-50px 0 0 0"/>
+
+            <Heading size={1} fit textColor="primary" margin="-100px 0 0 0">
+                React
+            </Heading>
         </Slide>
-        <Slide transition={["spin", "slide"]} bgColor="tertiary">
+
+        <Slide transition={["slide"]} bgColor="black">
+            <Heading size={1} caps fit textColor="primary">
+              Абстракция над DOM
+            </Heading>
+            <Text size={1} lineHeight={1} margin="20 0 0 0" textColor="tertiary">что избавило от необходимости использовать шаблонизаторы</Text>
+            {/*<Heading size={1} caps fit textColor="tertiary">*/}
+        </Slide>
+
+        <Slide transition={["slide"]} bgColor="black">
+            <Heading size={1} caps fit textColor="primary">
+              Компоненты
+            </Heading>
+            <Heading size={1} caps fit textColor="primary">
+              и их композиция
+            </Heading>
+            <Text size={1} lineHeight={1} margin="20 0 0 0" textColor="tertiary">что позволило создавать по настоящему реиспользуемые функциональные блоки приложения</Text>
+        </Slide>
+
+        <Slide transition={["slide"]} bgColor="black">
+            <Heading size={1} caps fit textColor="primary">
+              Декларативность
+            </Heading>
+            <Heading size={1} lineHeight={1.5} caps fit textColor="primary">
+              и идемпотентность
+            </Heading>
+        </Slide>
+
+        <Slide transition={["slide"]} bgColor="black">
+            <Heading size={1} caps fit textColor="primary">
+              Функциональность
+            </Heading>
+        </Slide>
+
+        <Slide transition={["slide"]} bgColor="black">
+            <Heading size={1} caps fit textColor="primary">
+              Изоморфность
+            </Heading>
+            <Heading size={1} caps fit textColor="primary">
+              она же Универсальность
+            </Heading>
+        </Slide>
+
+        <Slide transition={["slide"]} bgColor="black">
+            <Heading size={1} caps fit textColor="primary">
+              Различные compile targets
+            </Heading>
+            <Heading size={1} caps fit textColor="primary">
+              и различные compile source
+            </Heading>
+        </Slide>
+
+        <Slide transition={["zoom", "fade"]} bgColor="primary">
+            <Image src={images.flux} height="400px" margin="-150px 0 0 0"/>
+
+            <Heading size={1} fit margin="-100px 0 0 0">
+                Flux
+            </Heading>
+        </Slide>
+
+        <Slide transition={["zoom", "fade"]} bgColor="primary">
+            <Heading size={1} caps fit>
+                Предсказуемость
+            </Heading>
+        </Slide>
+
+        <Slide transition={["zoom", "fade"]} bgColor="primary">
+            <Heading size={1} caps fit>
+                Однонаправленность
+            </Heading>
+            <Heading size={1} caps fit>
+                потока данных
+            </Heading>
+        </Slide>
+
+        <Slide transition={["zoom", "fade"]} bgColor="primary">
+            <Heading size={1} caps fit>
+                Отсутствие two-way binding
+            </Heading>
+        </Slide>
+
+        <Slide transition={["zoom", "fade"]} bgColor="primary">
+            <Heading size={1} caps fit>
+                Стало проще понять
+            </Heading>
+            <Heading size={1} caps fit>
+                как приложение устроено
+            </Heading>
+        </Slide>
+
+        <Slide transition={["zoom", "fade"]} bgColor="primary">
+            <Heading size={1} caps fit>
+                Undo/Redo и Report bug
+            </Heading>
+            <Heading size={1} caps fit>
+                из коробки
+            </Heading>
+        </Slide>
+
+        <Slide transition={["spin", "slide"]} bgColor="black">
           <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-            Made with love in Seattle by
+            THANKS
           </Heading>
-          <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
         </Slide>
       </Deck>
     );
